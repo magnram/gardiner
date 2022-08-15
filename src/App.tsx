a5b221f1198ae7a19e89b656169e5b8f682639e2
Magnus Ramm <magnus.ramm@bekk.no>
2022-08-15 15:16:17 +0200

    Disables scrolling? test

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,13 @@ import { useEffect } from "react";
 const App = () => {
   useEffect(() => {
     document.body.style.overflow = "hidden";
+    window.addEventListener("scroll", (e) => {
+      e.preventDefault();
+      window.scrollTo(0, 0);
+    });
+
   }, []);
+  
   return (<div className="App">
     Hey
   </div>
