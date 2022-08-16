859969772d0548404c08340eebb1ca18aba79d56
Magnus Ramm <magnus.ramm@bekk.no>
2022-08-16 11:56:12 +0200

    Get curtain state from backend

--- src/components/SlidableWindow/SlidableWindow.tsx
+++ src/components/SlidableWindow/SlidableWindow.tsx
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 import { Slider } from '@mui/material';import "./SlidableWindow.scss"
 
-const SlidableWindow = () => {
-    const valuetext = (value: number) => `${value}°C`;
-    const marks = [{value: 0}, {value: 100} ];
+interface Props {
+    id: number,
+    pos1: number, 
+    pos1Destination: number, 
+    pos2: number, 
+    pos2Destination: number,
+    key: any
+}
+
+const SlidableWindow = ({pos1, pos1Destination, pos2, pos2Destination}: Props) => {
+    const valuetext = (value: number) => `${value}%`;
+    const marks = [{value: pos1}, {value: pos2} ];
 
     return (
         <div className="SlidableWindow">
@@ -11,7 +20,7 @@ const SlidableWindow = () => {
                 getAriaLabel={() => 'Temperature'}
                 orientation="vertical"
                 getAriaValueText={valuetext}
-                defaultValue={[20, 37]}
+                defaultValue={[pos1, pos2]}
                 valueLabelDisplay="auto"
                 marks={marks}
             />
--- src/components/WindowWrapper/WindowWrapper.tsx
+++ src/components/WindowWrapper/WindowWrapper.tsx
@@ -2,22 +2,27 @@ import { useEffect, useState } from "react";
 import SlidableWindow from "../SlidableWindow/SlidableWindow";
 import "./WindowWrapper.scss"
 
+interface WindowData {
+    id: number,
+    pos1: number, 
+    pos1Destination: number, 
+    pos2: number, 
+    pos2Destination: number
+}
+
 const WindowWrapper = () => {
-    
+    const [data, setData] = useState<WindowData[]>();
     useEffect(() => {
         fetch("http://localhost:8080/getAll")
           .then((response) => response.json())
           .then((data) => {
-              console.log(data)
+              setData(data);
           });
       }, []);
-    
+
     return (
         <div className="WindowWrapper">
-            <SlidableWindow />
-            <SlidableWindow />
-            <SlidableWindow />
-            <SlidableWindow />
+            {data ? data.map((a, idx) => <SlidableWindow {...a} key={idx} />) : null}
         </div>
     );
 };
