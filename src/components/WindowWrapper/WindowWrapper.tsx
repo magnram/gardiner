5e1ef2fda664e174b766dd9cf9760e3ae31ad4b6
Magnus Ramm <magnus.ramm@bekk.no>
2022-08-16 10:57:12 +0200

    Add backend request to window wrapper, console logged

--- src/components/WindowWrapper/WindowWrapper.tsx
+++ src/components/WindowWrapper/WindowWrapper.tsx
@@ -1,16 +1,25 @@
-// import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import SlidableWindow from "../SlidableWindow/SlidableWindow";
 import "./WindowWrapper.scss"
 
 const WindowWrapper = () => {
-  return (
-    <div className="WindowWrapper">
-        <SlidableWindow />
-        <SlidableWindow />
-        <SlidableWindow />
-        <SlidableWindow />
-    </div>
-    )
+    
+    useEffect(() => {
+        fetch("http://localhost:8080/getAll")
+          .then((response) => response.json())
+          .then((data) => {
+              console.log(data)
+          });
+      }, []);
+    
+    return (
+        <div className="WindowWrapper">
+            <SlidableWindow />
+            <SlidableWindow />
+            <SlidableWindow />
+            <SlidableWindow />
+        </div>
+    );
 };
 
 export default WindowWrapper;
