17f502fda91c08b78bcc87c02d4175a1e86a8704
Magnus Ramm <magnus.ramm@bekk.no>
2022-09-01 12:21:48 +0200

    Added weather to windows

--- src/components/Window/Window.tsx
+++ src/components/Window/Window.tsx
@@ -1,8 +1,34 @@
 import { useEffect, useState } from "react";
+import { getBaseUrl } from "../..";
 import "./Window.scss"
 
-const url = "https://api.met.no/images/weathericons/svg/clearsky_polartwilight.svg";
+
 const Window = ({weather}: {weather: boolean}) => {
+    const lat = 19.9138204;
+    const lng = 30.752245;
+    const [location, setLocation] = useState<{lat: number, lng: number}>();
+    const [url, setUrl] = useState<string>();
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            position => setLocation({ 
+              lat: position.coords.latitude, 
+              lng: position.coords.longitude
+            }), 
+            err => console.log(err)
+        );
+    }, []);
+    useEffect(() => {
+        if(weather && location) {
+            fetch(`${getBaseUrl()}/yr/getWeather?lat=${location.lat}&lng=${location.lng}`)
+            .then(res => res.text())
+            .then(data => {
+                setUrl(data);
+            })
+            
+        }
+    }, [location])
+    
     return (
         <div className="Window">
             <div className="weatherLayer" style={ weather ? { backgroundImage: `url(${url})` } : {}}></div>
